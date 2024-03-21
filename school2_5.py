528c46de59c3663ceb4521ec2c06c27413bc6573
samnyongyi <163753650+samnyongyi@users.noreply.github.com>
2024-03-21 18:28:17 +0900

    Add files via upload

---
+++ school2_5.py
@@ -0,0 +1,37 @@
+a=0xff
+b=0o77
+c=0b1111
+print(a,b,c)
+
+a=3.14
+b=3.14e5
+print(a,b)
+
+a=10; b=20
+print(a+b,a-b,a*b,a/b)
+
+a,b=9,2
+print(a**b,a%b,a//b)
+
+a=True
+type(a)
+
+a=(100==100)
+b=(10>100)
+print(a,b)
+
+a="파이썬 만세"
+a
+print(a)
+type(a)
+
+a="작은따옴표는 '모양이다"
+b='큰따옴표는 "모양이다'
+print(a,b)
+
+print("%s\n%s" %(a,b))
+
+a="""파이썬
+만세"""
+a
+print(a)
